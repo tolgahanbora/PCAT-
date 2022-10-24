1743b53e2a9b19ac89a00ebc7aab546ddfa390c8
tolgahanbora <tolgahanbora@stu.aydin.edu.tr>
2022-10-25 01:26:33 +0300

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import {getPhotoUpload,getPhotoEdit,getPhotoDelete} from '../PCAT/Controller/Pho
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 //MONGOOSE CONNECT
 mongoose.connect(`${process.env.DB_URI}`).then(() => console.log("mongodb CONNECTED")).catch((err) => console.log("err"))
 
